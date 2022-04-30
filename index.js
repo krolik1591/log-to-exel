933dbd21b11f438db981611e8178ae7aea0705c6
krolik <dasenkosasa19@gmail.com>
2022-04-30 19:44:40 +0300

    rewrited code

--- index.js
+++ index.js
@@ -1,25 +1,72 @@
 import * as fs from 'fs';
 import * as readline from 'readline';
 
-let a = await processLineByLine();
-let set = new Set()
-let str = ''
-let obj = {};
-
 main();
 
 async function main() {
-    lengthOfKeys()
-    startKeys()
-    keyToValue()
+    let str = ''
+    let arr = await processLineByLine('E:\\work\\test.txt')
+    let obj = setToObject(setOfKeys(arr))
+
+    str = objectKeysToString(obj) + addValueToString(arr, obj)
 
     let file = await fs.promises.open("E:\\work\\tabl.csv", 'a')
     await fs.promises.appendFile(file, str);
     await file.close()
 }
 
-async function processLineByLine() {
-    const fileStream = fs.createReadStream('E:\\work\\test.txt');
+function addValueToString(arr, obj) {
+    let str = '';
+
+    for (let part of arr) {
+        let objRight = obj
+
+        for (let [key, value] of Object.entries(part)) {
+            objRight[key] = value
+        }
+        str += objectValuesToString(objRight) + '\n'
+    }
+    return str
+}
+
+function objectKeysToString(obj) {
+    let str = ''
+    for (let key of Object.keys(obj)) {
+        str += key + ','
+    }
+    return str + '\n'
+}
+
+function objectValuesToString(obj) {
+    let str = ''
+    for (let value of Object.values(obj)) {
+        str += value + ','
+    }
+    return str
+}
+
+function setToObject(set) {
+    let obj = {}
+    for (let key of set) {
+        obj[key] = ''
+    }
+    return obj
+}
+
+function setOfKeys(arr) {
+    let set = new Set();
+
+    for (let part of arr) {
+        let keys = Object.keys(part);
+        for (let key of keys) {
+            set.add(key)
+        }
+    }
+    return set
+}
+
+async function processLineByLine(Path) {
+    const fileStream = fs.createReadStream(Path);
 
     const rl = readline.createInterface({input: fileStream,});
 
@@ -29,48 +76,4 @@ async function processLineByLine() {
         a.push(JSON.parse(x['log']));
     }
     return a;
-}
-
-function lengthOfKeys() {
-    for (let i = 0; i < a.length; i++) {
-        let arrKeys = []
-        for (let key in a[i]) {
-            arrKeys.push(key)
-        }
-
-        for (let j = 0; j < arrKeys.length; j++) {
-            let key = arrKeys[j];
-            set.add(key)
-        }
-    }
-}
-
-function startKeys() {
-    let pisya = Array.from(set)
-    for (let i = 0; i < pisya.length; i++) {
-        str += pisya[i] + ','
-    }
-    str += '\n'
-}
-
-function keyToValue() {
-    for (let i = 0; i < a.length; i++) {
-        let arr = a[i];
-
-        for (let item of set) {
-            obj[item] = ''
-        }
-
-        let value = Object.values(arr);
-        let key = Object.keys(arr);
-
-        for (let j = 0; j < value.length; j++) {
-            obj[key[j]] += value[j]
-        }
-
-        for (let key in obj) {
-            str += obj[key] + ','
-        }
-        str += '\n'
-    }
 }
